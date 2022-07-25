4fda5b04fb3f054524ba2d80ae4f5b15d8256150
Lucas John <60789246+lucasojohn@users.noreply.github.com>
2022-07-24 21:19:26 -0300

    Second Commit

--- Agencia.java
+++ Agencia.java
@@ -3,9 +3,8 @@ package banco;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Agencia {
-	
+
 	Map<Integer, Conta> mapaDeContas = new HashMap<>();
 	Map<String, Cliente> mapaDeClientes = new HashMap<>();
 	private int numeroAG;
@@ -14,45 +13,43 @@ public class Agencia {
 		this.numeroAG = numeroAG;
 		cadastrarCliente("123", "joao", "silva");
 	}
-        
-        public void cadastrarCliente(String cpf, String nome, String sobrenome) {
-		
+
+	public void cadastrarCliente(String cpf, String nome, String sobrenome) {
+
 		mapaDeClientes = new HashMap<>();
 		Cliente cliente = new Cliente(cpf, nome, sobrenome);
 		mapaDeClientes.put(cpf, cliente);
 	}
-        
-        public void cadastrarConta(Cliente titular, int numeroConta) {
-		
+
+	public void cadastrarConta(Cliente titular, int numeroConta) {
+
 		mapaDeContas = new HashMap<>();
 		Conta conta = new ContaCorrente(titular, numeroConta);
 		mapaDeContas.put(numeroConta, conta);
 	}
-        
+
 	public Map<Integer, Conta> getMapaDeContas() {
 		return mapaDeContas;
 	}
-	
+
 	public void setMapaDeContas(Map<Integer, Conta> mapaDeContas) {
 		this.mapaDeContas = mapaDeContas;
 	}
-	
+
 	public Map<String, Cliente> getMapaDeClientes() {
 		return mapaDeClientes;
 	}
-	
+
 	public void setMapaDeClientes(Map<String, Cliente> mapaDeClientes) {
 		this.mapaDeClientes = mapaDeClientes;
 	}
-	
+
 	public int getNumeroAG() {
 		return numeroAG;
 	}
-	
+
 	public void setNumeroAG(int numeroAG) {
 		this.numeroAG = numeroAG;
 	}
-	
-	
-	
+
 }
--- Main.java
+++ Main.java
@@ -1,28 +1,166 @@
+package banco;
+
 import java.util.Scanner;
 
-
 public class Main {
-
-	public static void main(String[] args) {
-		
-		Scanner s = new Scanner(System.in);
-		
-		// apresenta menu inicial para o usuário fazer a escolha de qual operação realizar 
-		MenuInicial menuInicial = new MenuInicial();
-		menuInicial.apresentarMenu();
 	
-		// usuario passa valor da opção desejada
-		int opcaoDesejada = s.nextInt();
+    public static void main(String[] args) {
 
-		// retorna menu da opção desejada
-		RetornaMenuDesejado retornaMenuDesejado = new RetornaMenuDesejado();
-		retornaMenuDesejado.retornaMenuEscolhido(opcaoDesejada);
+        Scanner s = new Scanner(System.in);
+        int opcaoDesejada;
+        
+        Agencia agencia = new Agencia(1234);
+        
+        do{
+            // apresenta menu inicial para o usuï¿½rio fazer a escolha de qual operaï¿½ï¿½o realizar 
+            Menu menu = new Menu();
+            menu.inicial();
+
+            // usuario passa valor da opï¿½ï¿½o desejada
+            opcaoDesejada = s.nextInt();
+
+            switch (opcaoDesejada) {
 		
-		// executa opção desejada
-		
-		
-		
-		
-	}
+                case 1:                    
+                    menu.gerirContas();
+                    opcaoDesejada = s.nextInt();
+                    
+                    do{
+                        switch(opcaoDesejada){                          
+                                
+                            case 1:
+                            	
+                            	System.out.println("Informe o CPF do cliente:");
+                            	String cpf = s.next();
+                            	
+                            	Cliente cliente = agencia.mapaDeClientes.get(cpf);
+                            	
+                            	try {
+                            		agencia.cadastrarConta(cliente, 123456);
+                            		System.out.println("Conta cadastrada\n");  
+                            		
+                            	} catch (NullPointerException e) {
+                            		System.out.println("Cliente não localizado, favor realizar cadastro do cliente\n");
+                            	}
+                            		
+                                break;
+                                
+                            case 2:
+                            	
+                            	System.out.println("Informe o numero da conta que deseja atualizar:");
+                            	int nrConta = s.nextInt();
+                            	
+                            	try {
+                            		
+                            		Conta conta = agencia.mapaDeContas.get(nrConta);
+                            		System.out.println("Informe novo saldo para a conta:");
+                            		double novoSaldo = s.nextDouble();
+                            		conta.setSaldo(novoSaldo);
+                            		System.out.println("Saldo atualizado com sucesso!");
+                            		
+                            	} catch(NullPointerException a) {
+                            		System.out.println("Conta informada nao existe");
+                            	}
+                            	
+                            	menu.gerirContas();
+                                opcaoDesejada = s.nextInt();
+                                break;
+                                
+                            case 3:
+                                break;
+                                
+                            case 4:
+                                break;
+                                
+                            case 5:
+                                break;
+                                
+                            case 6:
+                                break;
+                                
+                            default:
+                                System.out.println("Selecione uma opcaoo valida!\n\n");
+                                break;    
+                        }
+                    }while(opcaoDesejada != 6);
+                    break;
+
+                case 2:
+                    menu.gerirClientes();                    
+                    opcaoDesejada = s.nextInt();
+                    
+                    do{
+                        switch(opcaoDesejada){                          
+                                
+                            case 1:
+                                
+                                agencia.cadastrarCliente("444", "Pedro", "Pedroso");
+                                agencia.cadastrarConta(agencia.mapaDeClientes.get("444"), 130055);
+                                menu.gerirClientes();                    
+                                opcaoDesejada = s.nextInt();
+                                break;
+                                
+                            case 2:
+                                break;
+                                
+                            case 3:
+                                break;
+                                
+                            case 4:
+                                break;
+                                
+                            case 5:
+                                break;
+                                
+                            case 6:
+                                break;
+                                
+                            default:
+                                System.out.println("Selecione uma opcao valida!\n\n");
+                                break;    
+                        }
+                    }while(opcaoDesejada != 6);
+                    break;
+
+                case 3:
+                    menu.acoesConta();
+                    opcaoDesejada = s.nextInt();
+                    
+                    do{
+                        switch(opcaoDesejada){                          
+                                
+                            case 1:
+                                break;
+                                
+                            case 2:
+                                break;
+                                
+                            case 3:
+                                break;
+                                
+                            case 4:
+                                break;                             
+                                
+                            default:
+                                System.out.println("Selecione uma opcao valida!\n\n");
+                                break;    
+                        }
+                    }while(opcaoDesejada != 6);
+                    break;
+                
+                case 4:
+                    break;
+                    
+                case 5:
+                    break;
+                    
+                default:
+                    System.out.println("Selecione uma opcao valida!\n\n");
+                    break;
+            }
+   
+        }while(opcaoDesejada != 5);
+
+    }
 
 }
